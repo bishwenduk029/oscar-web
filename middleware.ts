6dd2fe3afae76492737be26874fc3fd8a0314830
Bishwendu Kundu <bishwenduk029@gmail.com>
2023-11-26 12:32:33 +0530

    Handle authenticated use for app session registration

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
+import { NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
 
 export default withAuth(
   async function middleware(req) {
     const token = await getToken({ req })
+    const appLoginID = req.nextUrl.searchParams.get("appLoginID")
     const isAuth = !!token
     const isAuthPage =
       req.nextUrl.pathname.startsWith("/login") ||
@@ -12,21 +13,26 @@ export default withAuth(
 
     if (isAuthPage) {
       if (isAuth) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
+        if (appLoginID) {
+          return NextResponse.redirect(
+            new URL(`/complete?appLoginID=${appLoginID}`, req.url)
+          )
+        }
+        return NextResponse.redirect(new URL("/", req.url))
       }
 
       return null
     }
 
     if (!isAuth) {
-      let from = req.nextUrl.pathname;
+      let from = req.nextUrl.pathname
       if (req.nextUrl.search) {
-        from += req.nextUrl.search;
+        from += req.nextUrl.search
       }
 
       return NextResponse.redirect(
         new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-      );
+      )
     }
   },
   {
